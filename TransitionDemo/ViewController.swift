3e19687f3d2d4b42ef5741cc52a50eca96e3dfbe
Lammert Westerhoff <westerhoff@gmail.com>
2015-07-06 12:25:12 +0200

    Initial Commit

---
+++ TransitionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransitionDemo
+//
+//  Created by Lammert Westerhoff on 06/07/15.
+//  Copyright (c) 2015 Xebia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
