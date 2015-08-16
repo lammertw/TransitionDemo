095f95e76f2b153757e1c6ae50e3b94d86e79f97
Lammert Westerhoff <westerhoff@gmail.com>
2015-08-16 20:44:36 +0200

    Redo code for animated

--- TransitionDemo/ViewControllers.swift
+++ TransitionDemo/ViewControllers.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    var selectedView: UIView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Colors"
@@ -17,6 +19,7 @@ class MainViewController: UIViewController {
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let button = sender as? UIButton, detailViewController = segue.destinationViewController as? DetailViewController {
+            selectedView = button.superview
             detailViewController.color = button.superview?.backgroundColor
             detailViewController.colorName = button.titleForState(.Normal)
             detailViewController.labelColor = button.titleLabel?.textColor
@@ -43,5 +46,81 @@ class DetailViewController: UIViewController {
         colorView.backgroundColor = color
         label.text = colorName
         label.textColor = labelColor
+    }
+}
+
+class Transitions: NSObject, UINavigationControllerDelegate {
+
+    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        let animations = ColorAnimations()
+        animations.reverse = operation == .Pop
+        return animations
+    }
+}
+
+class ColorAnimations: NSObject, UIViewControllerAnimatedTransitioning {
+
+    var reverse = false
+
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        return 0.4
+    }
+
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        // implement the transitions
+        if let mainViewController = transitionContext.viewControllerForKey(reverse ? UITransitionContextToViewControllerKey : UITransitionContextFromViewControllerKey) as? MainViewController,
+            let detailViewController = transitionContext.viewControllerForKey(reverse ? UITransitionContextFromViewControllerKey : UITransitionContextToViewControllerKey) as? DetailViewController {
+
+                transitionContext.containerView().addSubview(reverse ? mainViewController.view : detailViewController.view)
+
+                let animationView = UIView()
+                animationView.backgroundColor = mainViewController.selectedView!.backgroundColor
+                transitionContext.containerView().addSubview(animationView)
+
+
+                let animationLabel = UILabel()
+                let button = mainViewController.selectedView!.subviews[0] as! UIButton
+                animationLabel.font = UIFont.systemFontOfSize(15)
+                animationLabel.text = button.titleForState(.Normal)
+                animationLabel.textColor = button.titleLabel?.textColor
+                animationLabel.backgroundColor = UIColor.clearColor()
+                animationLabel.textAlignment = .Center
+                transitionContext.containerView().addSubview(animationLabel)
+
+                if !reverse {
+                    detailViewController.view.layoutIfNeeded()
+                    animationLabel.sizeToFit()
+                }
+
+                var sourceFrame = mainViewController.selectedView!.frame
+                var targetFrame = detailViewController.view.frame
+                var sourceLabelFrame = transitionContext.containerView().convertRect(button.frame, fromView: mainViewController.selectedView!)
+                var targetLabelFrame = detailViewController.label.frame
+
+                if reverse {
+                    swap(&sourceFrame, &targetFrame)
+                    swap(&sourceLabelFrame, &targetLabelFrame)
+                    animationLabel.frame = sourceLabelFrame
+                }
+
+                animationView.frame = sourceFrame
+                animationLabel.center = CGPointMake(CGRectGetMidX(sourceLabelFrame), CGRectGetMidY(sourceLabelFrame))
+
+                detailViewController.view.hidden = true
+
+                UIView.animateWithDuration(transitionDuration(transitionContext), animations: {
+                    animationView.frame = targetFrame
+                    animationLabel.center = CGPointMake(CGRectGetMidX(targetLabelFrame), CGRectGetMidY(targetLabelFrame))
+                    
+                    }) { finished in
+                        if finished {
+                            animationView.removeFromSuperview()
+                            animationLabel.removeFromSuperview()
+                            
+                            detailViewController.view.hidden = false
+                            transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+                        }
+                }
+        }
     }
 }
