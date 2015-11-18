b16d22b41e0525285c1ad36063849d887fbbf6fc
Lammert Westerhoff <westerhoff@gmail.com>
2015-11-18 22:49:13 +0100

    WIP

--- TransitionDemo/ViewControllers.swift
+++ TransitionDemo/ViewControllers.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    var selectedView: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +21,43 @@ class MainViewController: UIViewController {
             detailViewController.color = button.superview?.backgroundColor
             detailViewController.colorName = button.titleForState(.Normal)
             detailViewController.labelColor = button.titleLabel?.textColor
+            selectedView = button.superview
+
         }
     }
 
+    @IBAction func pinch(sender: UIPinchGestureRecognizer) {
+        switch (sender.state) {
+
+        case .Began:
+            transitions?.interactionController = UIPercentDrivenInteractiveTransition()
+
+            let button = sender.view as! UIButton
+            performSegueWithIdentifier(button.titleForState(.Normal), sender: button)
+        case .Changed:
+            transitions?.interactionController?.updateInteractiveTransition(sender.scale - 1)
+
+        default: // .Ended, .Cancelled, .Failed ...
+            if sender.velocity > 0 || (sender.scale > 1.5 && sender.velocity == 0) {
+                transitions?.interactionController?.finishInteractiveTransition()
+            } else {
+                transitions?.interactionController?.cancelInteractiveTransition()
+            }
+            transitions?.interactionController = nil
+        }
+
+
+    }
 
 }
+
+extension UIViewController {
+
+    var transitions: Transitions? {
+        return navigationController?.delegate as? Transitions
+    }
+}
+
 
 class DetailViewController: UIViewController {
 
@@ -43,5 +76,132 @@ class DetailViewController: UIViewController {
         colorView.backgroundColor = color
         label.text = colorName
         label.textColor = labelColor
+
+        if let transitions = transitions {
+            let edgePanGesture = UIScreenEdgePanGestureRecognizer(target: transitions, action: "pan:")
+            edgePanGesture.edges = .Left
+            view.addGestureRecognizer(edgePanGesture)
+        }
+
     }
 }
+
+class Transitions: NSObject, UINavigationControllerDelegate {
+
+    @IBOutlet weak var navigationController: UINavigationController!
+    
+    var interactionController: UIPercentDrivenInteractiveTransition?
+
+    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        // here we check if we need to animate the transition from fromVC to toVC and then return a Animated Transitioning object
+        let animations = ColorAnimations()
+        animations.reverse = operation == .Pop
+
+        return animations
+
+    }
+
+    func navigationController(navigationController: UINavigationController, interactionControllerForAnimationController animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactionController
+    }
+
+    func pan(recognizer: UIScreenEdgePanGestureRecognizer) {
+        let translation = recognizer.translationInView(recognizer.view!)
+        let d =  translation.x / CGRectGetWidth(recognizer.view!.bounds)
+
+        switch (recognizer.state) {
+
+        case .Began:
+            interactionController = UIPercentDrivenInteractiveTransition()
+            navigationController?.popViewControllerAnimated(true)
+
+        case .Changed:
+            interactionController?.updateInteractiveTransition(d)
+
+        default:
+
+            if recognizer.velocityInView(recognizer.view!).x > 0 {
+                interactionController?.finishInteractiveTransition()
+            } else {
+                interactionController?.cancelInteractiveTransition()
+            }
+            interactionController = nil
+        }
+    }
+
+}
+
+
+class ColorAnimations: NSObject, UIViewControllerAnimatedTransitioning {
+    var reverse = false
+
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        return 1.0
+    }
+
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        // implement the transition
+
+        if let mainViewController = transitionContext.viewControllerForKey(reverse ? UITransitionContextToViewControllerKey : UITransitionContextFromViewControllerKey) as? MainViewController,
+            let detailViewController = transitionContext.viewControllerForKey(reverse ? UITransitionContextFromViewControllerKey : UITransitionContextToViewControllerKey) as? DetailViewController {
+
+                transitionContext.containerView().addSubview(reverse ? mainViewController.view : detailViewController.view)
+
+                let animationView = UIView()
+                animationView.backgroundColor = mainViewController.selectedView!.backgroundColor
+                transitionContext.containerView().addSubview(animationView)
+
+
+                let animationLabel = UILabel()
+                let button = mainViewController.selectedView!.subviews[0] as! UIButton
+                animationLabel.font = UIFont.systemFontOfSize(15)
+                animationLabel.text = button.titleForState(.Normal)
+                animationLabel.textColor = button.titleLabel?.textColor
+                animationLabel.backgroundColor = UIColor.clearColor()
+                animationLabel.textAlignment = .Center
+                transitionContext.containerView().addSubview(animationLabel)
+
+                if !reverse {
+                    detailViewController.view.layoutIfNeeded()
+                    animationLabel.sizeToFit()
+                }
+
+                var sourceFrame = mainViewController.selectedView!.frame
+                var targetFrame = detailViewController.view.frame
+                var sourceLabelFrame = transitionContext.containerView().convertRect(button.frame, fromView: mainViewController.selectedView!)
+                var targetLabelFrame = detailViewController.label.frame
+
+                if reverse {
+                    swap(&sourceFrame, &targetFrame)
+                    swap(&sourceLabelFrame, &targetLabelFrame)
+                    animationLabel.frame = sourceLabelFrame
+                }
+
+                animationView.frame = sourceFrame
+                animationLabel.center = CGPointMake(CGRectGetMidX(sourceLabelFrame), CGRectGetMidY(sourceLabelFrame))
+
+                detailViewController.view.hidden = true
+                
+                UIView.animateWithDuration(transitionDuration(transitionContext), animations: {
+                    animationView.frame = targetFrame
+                    animationLabel.center = CGPointMake(CGRectGetMidX(targetLabelFrame), CGRectGetMidY(targetLabelFrame))
+                    
+                    }) { finished in
+                        if finished {
+                            animationView.removeFromSuperview()
+                            animationLabel.removeFromSuperview()
+                            
+                            detailViewController.view.hidden = false
+                            transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+                        }
+                }
+
+
+
+        }
+
+    }
+
+    
+}
+
